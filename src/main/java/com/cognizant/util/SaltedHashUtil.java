6c90a2711e1a6fcd6d9f1bd12aab6653b58ebdd0
ssinha6 <sourav6.sinha6@cognizant.com>
2016-10-18 19:09:52 +0530

    Flows updated, Postman test cases added

--- src/main/java/com/cognizant/util/SaltedHashUtil.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.util;
-
-public class SaltedHashUtil {
-
-	public String generateSaltedHash(final String password){
-		System.out.println("we have recvd a value = " + password);
-		return password + "SaltedHash";
-	}
-}
