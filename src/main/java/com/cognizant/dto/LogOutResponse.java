521b1e48fe41192a2be2ca81de4965dc7b2fe556
ssinha6 <sourav6.sinha6@cognizant.com>
2016-10-19 15:48:41 +0530

    LoginLogout Flow added alongwith other modificatns

---
+++ src/main/java/com/cognizant/dto/LogOutResponse.java
@@ -0,0 +1,15 @@
+package com.cognizant.dto;
+
+public class LogOutResponse {
+
+	private boolean status;
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+}
